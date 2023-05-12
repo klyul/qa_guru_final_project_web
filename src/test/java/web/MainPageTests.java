ba3d74b4338d4f2ecb243b13714f864d52eed042
klyul <yuliya.v3333@gmail.com>
2023-05-12 21:22:36 +0300

    add Tests

--- src/test/java/web/MainPageTests.java
+++ src/test/java/web/MainPageTests.java
@@ -1,13 +1,17 @@
 package web;
 
+import com.codeborne.selenide.Condition;
 import components.ContactUsForm;
 import components.TopMenu;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import pages.MainPage;
 
-import static com.codeborne.selenide.Selenide.closeWindow;
+import static com.codeborne.selenide.Selenide.*;
 
 public class MainPageTests extends TestBaseBint {
 
@@ -35,5 +39,24 @@ public class MainPageTests extends TestBaseBint {
     @AfterEach
     void tearDown() {
         closeWindow();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"акция", "новая", "услуг", "проверки", "условия", "команде", "тестирования", "клиентский", "опыт", "система"})
+    void bellSearchTest(String value) {
+
+        mainPage.openPage();
+        $("#search-open").shouldBe(Condition.interactable);
+        $("#search-open").click();
+
+        $("input[name=search]").sendKeys(value);
+        $("input[name=search]").pressEnter();
+
+        $$("h2").first().shouldHave(Condition.text("Поиск - " + value));
+        $$("h2").get(1).shouldHave(Condition.text("Результаты поиска"));
+
+        int warnings = $$x("//b[contains(text(),'Warning')]").size();
+        Assertions.assertEquals(0, warnings, "Не должно быть PHP warnings");
+
     }
 }
