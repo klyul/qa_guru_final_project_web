b40479c5851bc1de1ad497d55ac9b8847b66a39a
klyul <yuliya.v3333@gmail.com>
2023-05-16 21:38:43 +0300

    add fixed selenide private

--- src/test/java/components/ContactUsForm.java
+++ src/test/java/components/ContactUsForm.java
@@ -6,12 +6,11 @@ import static com.codeborne.selenide.Condition.interactable;
 import static com.codeborne.selenide.Selenide.$;
 
 public class ContactUsForm {
-    SelenideElement formTitle = $(".stay-form_title");
-
-    SelenideElement firstNameInput = $("input[name=first_name]"),
-            lastNameInput = $("input[name=last_name]"),
-            emailInput = $("input[name=email]"),
-            sendFormButton = $("input#button_stay_connected");
+    private final SelenideElement formTitle = $(".stay-form_title");
+    private final SelenideElement firstNameInput = $("input[name=first_name]");
+    private final SelenideElement lastNameInput = $("input[name=last_name]");
+    private final SelenideElement emailInput = $("input[name=email]");
+    private final SelenideElement sendFormButton = $("input#button_stay_connected");
 
     @Step("Проверяем главные элементы формы")
     public void checkRequiredFormElements() {
--- src/test/java/components/TopMenu.java
+++ src/test/java/components/TopMenu.java
@@ -7,15 +7,16 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class TopMenu {
-    SelenideElement careerHeaeder = $x("//span[@class='top-menu_item_title']/span[contains(text(),'Карьера')]");
-    SelenideElement menuItemJobs = $x("//li/a[contains(text(),'Вакансии')]"),
-            menuItemRecruiters = $x("//li/a[contains(text(),'Обучение')]"),
-            menuItemTeaching = $x("//li/a[contains(text(),'Рекрутерам')]"),
-            menuItemBringAFriend = $x("//li/a[contains(text(),'Акция \"Приведи друга\"')]");
+    private final SelenideElement careerHeader = $x("//span[@class='top-menu_item_title']/span[contains(text(),'Карьера')]");
+    private final SelenideElement menuItemJobs = $x("//li/a[contains(text(),'Вакансии')]");
+    private final SelenideElement menuItemRecruiters = $x("//li/a[contains(text(),'Обучение')]");
+    private final SelenideElement menuItemTeaching = $x("//li/a[contains(text(),'Рекрутерам')]");
+    private final SelenideElement menuItemBringAFriend = $x("//li/a[contains(text(),'Акция \"Приведи друга\"')]");
 
 
     @Step("Проверяем что присутствуют элементы меню Карьера, Вакансии, Обучение, Рекрутерам, Акция Приведи Друга")
     public void menuItemsShouldBePresent() {
+        careerHeader.shouldBe(visible);
         menuItemJobs.shouldBe(visible);
         menuItemJobs.shouldBe(interactable);
         menuItemRecruiters.shouldBe(interactable);
